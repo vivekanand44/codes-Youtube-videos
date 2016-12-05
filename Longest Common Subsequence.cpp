1d5051f02702e0866e14da0f370049b03f60aa7f
vivek <khyade.vivekanand@gmail.com>
2016-12-05 18:16:05 +0530

    Longest Common Subsequence Dynamic Programming

--- Longest Common Subsequence.cpp
+++ Longest Common Subsequence.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 int main()
 {	
-	string str1 = "abcdefgh";
-	string str2 = "pqbrcueg";
-	
+	string str1 = "abcvdefgh";
+	string str2 = "bqdrcvefgh";
 	int m , n;
-	char lcs[10];
 	m = str1.length();
 	n = str2.length();
-	
 	int a[m+1][n+1];
-	
 	for(int i=0;i<m+1;i++)
 	{
 		for(int j=0;j<n+1;j++)
@@ -26,11 +22,10 @@ int main()
 	{
 		for(int j=1;j<n+1;j++)
 		{
-			if(str1[i] == str2[j])
+			if(str1[i-1] == str2[j-1])
 			{
-				a[i][j] = a[i-1][j-1] + 1;
-				lcs[k] = str1[i];
-				k++;				
+				a[i][j] = a[i-1][j-1] + 1;				
+							
 			}		
 			else
 			{
@@ -45,9 +40,35 @@ int main()
 			}
 		}
 	}
+	cout <<"Length of Longest Common Subsequence is" << a[m][n];
+	//Now let us find out the Longest common subsequence //
+	int i = m;
+	int j = n;
+	char lcs[a[m][n]];
+	k = a[m][n]-1;
 	lcs[k] = '\0';
-	cout << lcs;
-
+	while(i > -1 && j > -1)
+	{
+		if(str1[i-1] == str2[j-1])
+		{	
+			lcs[k] = str1[i-1];
+			i--;
+			j--;
+			k--;
+		}
+		else
+		{
+			if(a[i-1][j] > a[i][j-1])
+				{
+					i = i-1;
+				}
+			else
+				{
+					j = j-1;
+				}
+		}
+	}
+	cout <<"\n\n The Longest Common Subsequence is=> " << lcs;
 return 0;
 }
 
